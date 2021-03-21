82debfe67274d89844d6567366fc7bef345e99d1
Charles-Svg <78506028+Charles-Svg@users.noreply.github.com>
2021-03-21 15:29:29 +0100

    Update database.php

--- modele/database.php
+++ modele/database.php
@@ -5,7 +5,7 @@ class ModeleBDD // class utilisée pour se co a la BDD
 
     // contructeur instantie le pdo
     function __construct() {
-      $config = parse_ini_file('../config.ini');
+      $config = parse_ini_file('config.ini');
       $dsn = "{$config["bddtype"]}:dbname={$config["bddname"]};host={$config["bddhost"]}";
       $user = $config["bdduname"];
       $password = $config["bddupass"];
@@ -16,27 +16,15 @@ class ModeleBDD // class utilisée pour se co a la BDD
     }
 
     private function init_bdd() {
-      $this->bdd->query($file_get_contents('../BDD/convsport.sql'));
+      $this->bdd->query($file_get_contents('BDD/convsport.sql'));
     }
 
-    //fonction par requete : QueryAdmin => retourne le tableau des admins (mdp id ect)
-    private function getBdd()
-    {
-        if($this->bdd==null)
-        {
-            //creation du pdo
-            $this->bdd = new PDO('mysql:host=localhost;port=3308;dbname=convsport;',       
-            'root', '',
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));     
-             }
-        return $this->bdd;
-    } 
 
     public function getAdmin() //retourne la table des id + mdp 
     {
         $qry='SELECT * from admin';
         
-        return $this->getBdd()->query($qry);
+        return $this->bdd->query($qry);
     }
 
 
