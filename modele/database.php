a0d34a2963ab6ac9e785352e3fda7be43fe39aa1
StrategFirst <mathieu.toulon@free.fr>
2021-03-21 15:50:40 +0100

    ajout du port

--- modele/database.php
+++ modele/database.php
@@ -6,7 +6,7 @@ class ModeleBDD // class utilisée pour se co a la BDD
     // contructeur instantie le pdo
     function __construct() {
       $config = parse_ini_file('config.ini');
-      $dsn = "{$config["bddtype"]}:dbname={$config["bddname"]};host={$config["bddhost"]}";
+      $dsn = "{$config["bddtype"]}:dbname={$config["bddname"]};host={$config["bddhost"]};port={$config["bddport"]}";
       $user = $config["bdduname"];
       $password = $config["bddupass"];
       $this->bdd = new PDO($dsn,$user,$password);
