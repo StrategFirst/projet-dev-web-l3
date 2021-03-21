5079cb2f510c4344951f7a0d0abbc6dc97b6de6b
StrategFirst <mathieu.toulon@free.fr>
2021-03-21 15:44:52 +0100

    fix broken git

--- modele/database.php
+++ modele/database.php
@@ -19,25 +19,12 @@ class ModeleBDD // class utilisée pour se co a la BDD
       $this->bdd->query($file_get_contents('BDD/convsport.sql'));
     }
 
-<<<<<<< HEAD
     public function getAdmin() {
       $Query = "SELECT * FROM admin";
       return $this->bdd->query($Query)->fetchAll(PDO::FETCH_ASSOC);
     }
-    
-    //fonction par requete : QueryAdmin => retourne le tableau des admins (mdp id ect)
-    private function getBdd()
-=======
-
-    public function getAdmin() //retourne la table des id + mdp 
->>>>>>> 82debfe67274d89844d6567366fc7bef345e99d1
-    {
-        $qry='SELECT * from admin';
-        
-        return $this->bdd->query($qry);
-    }
 
 
-}; 
+};
 
 ?>
