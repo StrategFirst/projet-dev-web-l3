e5cbc4b58ca268a3872c316627f0c20de32ab41e
StrategFirst <mathieu.toulon@free.fr>
2021-03-21 15:03:05 +0100

    DataBase init

--- modele/database.php
+++ modele/database.php
@@ -1,12 +1,23 @@
-<?php 
-
-
+<?php
 class ModeleBDD // class utilisée pour se co a la BDD
 {
     private $bdd;
 
-    // contructeur instantie le pdo 
+    // contructeur instantie le pdo
+    function __construct() {
+      $config = parse_ini_file('../config.ini');
+      $dsn = "{$config["bddtype"]}:dbname={$config["bddname"]};host={$config["bddhost"]}";
+      $user = $config["bdduname"];
+      $password = $config["bddupass"];
+      $this->bdd = new PDO($dsn,$user,$password);
+      if( ! $this->test = $this->bdd->query('SELECT * FROM admin') ) {
+        $this->init_bdd();
+      }
+    }
 
+    private function init_bdd() {
+      $this->bdd->query($file_get_contents('../BDD/convsport.sql'));
+    }
 
     //fonction par requete : QueryAdmin => retourne le tableau des admins (mdp id ect)
     private function getBdd()
@@ -16,7 +27,7 @@ class ModeleBDD // class utilisée pour se co a la BDD
             //creation du pdo
         }
         return $bdd;
-    } 
-}; 
+    }
+};
 
 ?>
