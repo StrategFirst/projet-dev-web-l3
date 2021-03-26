2685abd678cff53b1d734347dfee2dfef891778a
StrategFirst <mathieu.toulon@free.fr>
2021-03-26 13:47:36 +0100

    passage de if else if a switch case

--- controlleur/MainControlleur.php
+++ controlleur/MainControlleur.php
@@ -5,6 +5,7 @@ require_once "controlleur/Controlleur_consult.php";
 require_once "controlleur/Controlleur_admin.php";
 require_once "controlleur/Controlleur_effectif.php";
 require_once "controlleur/Controlleur_rencontre.php";
+require_once "controlleur/Controlleur_convocation.php";
 
 class Main {
 
@@ -21,44 +22,48 @@ class Main {
     {
         if(isset($_GET['action']))
         {
-            if($_GET['action']=='connexion')
-             {
-                 $this->controlleur= new Controlleur_connexion();
-                 $this->controlleur->affichage();
-             }
-             if($_GET['action']=='consult')
-             {
-                $this->controlleur= new Controlleur_consult();
-                $this->controlleur->affichage();
-             }
-             if($_GET['action']=='admin')
-             {
-                $this->controlleur= new Controlleur_admin();
-                $this->controlleur->affichage();
-             }
-             if($_GET['action']=='effectif')
-             {
-                $this->controlleur = new Controlleur_effectif();
-                $this->controlleur->update();
-                $this->controlleur->affichage();
-             }
-             if($_GET['action']=='rencontres')
-             {
-                if($_GET['mode']=='lecture')
-                {
-                      console_log("action : rencotre mode :lecture");
-                      $this->page=new Controlleur_rencontre();
-                      $this->page->affiche_lecture();
-                }
-                else if($_GET['mode']=='edition')
-                {
-                    console_log("action : rencotre mode :edition");
-                    $this->page=new Controlleur_rencontre();
-                    $this->page->affiche_edition();
+          switch($_GET['action']) {
+            case 'convocation':
+              $this->controlleur= new Controlleur_convocation();
+              $this->controlleur->affichage();
+            break;
 
-                }
-             }
+            case 'connexion':
+              $this->controlleur= new Controlleur_connexion();
+              $this->controlleur->affichage();
+            break;
 
+            case 'consult':
+              $this->controlleur= new Controlleur_consult();
+              $this->controlleur->affichage();
+            break;
+
+            case 'admin':
+              $this->controlleur= new Controlleur_admin();
+              $this->controlleur->affichage();
+            break;
+
+            case 'effectif':
+              $this->controlleur = new Controlleur_effectif();
+              $this->controlleur->update();
+              $this->controlleur->affichage();
+            break;
+
+            case 'reoncontres':
+               if($_GET['mode']=='lecture')
+               {
+                     console_log("action : rencotre mode :lecture");
+                     $this->page=new Controlleur_rencontre();
+                     $this->page->affiche_lecture();
+               }
+               else if($_GET['mode']=='edition')
+               {
+                   console_log("action : rencotre mode :edition");
+                   $this->page=new Controlleur_rencontre();
+                   $this->page->affiche_edition();
+                 }
+            break;
+          }
         }
         else
         {
